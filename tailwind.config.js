d64ac27a4c1a6b5403065a8afa10d9012ccb3a61
hoodaddeveloper <mohammadkhaffas@gmail.com>
2024-10-23 18:54:55 +0200

    personalize tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,12 @@ module.exports = {
   content: ['./src/**/*.{html,js}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    /* Screen breakpoints customization*/
+    /* screens: {
+      'sm': '576px',
+      'md': '800px',
+      'lg': '1050px'
+    }, */
   },
   plugins: [],
 }
-
